411d9e2fae9f7bd4829ee56e9550e164343f27cf
Alba Mateos <albmatcar@gmail.com>
2020-12-10 12:39:39 +0000

    solved login with existing user

--- fe-nc-news-alba/src/Components/NavBar.jsx
+++ fe-nc-news-alba/src/Components/NavBar.jsx
@@ -22,7 +22,8 @@ class NavBar extends React.Component {
 
   handleClick = () => {
     this.setState((currState) => ({
-      isToggleOn: !currState.isToggleOn
+      isToggleOn: !currState.isToggleOn,
+      hasError: false
     }));
   };
 
@@ -34,51 +35,62 @@ class NavBar extends React.Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    this.props.logIn(this.state.username);
-    this.setState({ isToggleOn: true });
+
+    const userExists = this.state.users.filter((user) => {
+      return user.username === this.state.username;
+    });
+
+    if (userExists.length !== 0) {
+      this.props.logIn(this.state.username);
+      this.setState({ isToggleOn: true, hasError: false });
+    } else {
+      this.setState({
+        hasError: true,
+        errorMessage: 'There is no registered user with that name, try again!'
+      });
+    }
   };
 
   render() {
+    console.log(this.state.users);
     const { hasError, errorMessage, isToggleOn, username } = this.state;
     const { logOut, loggedUser } = this.props;
-    if (hasError) {
-      return <ErrorMessage errorMessage={errorMessage} />;
-    } else {
-      return (
-        <nav className='NavBar'>
-          {loggedUser ? (
-            <button className='NavBarButton' onClick={logOut}>
-              LogOut
-            </button>
-          ) : isToggleOn ? (
-            <button className='NavBarButton' onClick={this.handleClick}>
-              LogIn
-            </button>
-          ) : (
-            <div className='FormLoginDiv'>
-              <form className='FormLogin' onSubmit={this.handleSubmit}>
-                <input
-                  className='FormInputLogin'
-                  type='text'
-                  id='username'
-                  name='username'
-                  value={username}
-                  placeholder='username'
-                  required
-                  onChange={this.handleChange}
-                ></input>
-                <button className='FormButton' type='submit'>
-                  send
-                </button>
-              </form>
-            </div>
-          )}
-          <p className={loggedUser ? null : 'hideWelcome'}>
-            Welcome {loggedUser}
-          </p>
-        </nav>
-      );
-    }
+
+    return (
+      <nav className='NavBar'>
+        {loggedUser ? (
+          <button className='NavBarButton' onClick={logOut}>
+            LogOut
+          </button>
+        ) : isToggleOn ? (
+          <button className='NavBarButton' onClick={this.handleClick}>
+            LogIn
+          </button>
+        ) : (
+          <div className='FormLoginDiv'>
+            <form className='FormLogin' onSubmit={this.handleSubmit}>
+              <input
+                className='FormInputLogin'
+                type='text'
+                id='username'
+                name='username'
+                value={username}
+                placeholder='username'
+                required
+                onChange={this.handleChange}
+              ></input>
+              <button className='FormButton' type='submit'>
+                send
+              </button>
+            </form>
+          </div>
+        )}
+        <p className={loggedUser ? null : 'hideWelcome'}>
+          Welcome {loggedUser}
+        </p>
+        {hasError ? <ErrorMessage errorMessage={errorMessage} /> : null}
+      </nav>
+    );
   }
 }
 
